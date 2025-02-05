820425d1ab27e04c624d13bc3d34c8fb4cb1026b
Rishi Gurnani <rgur@Rishis-MacBook-Air-2.local>
2025-02-05 14:56:48 -0500

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Dr. Rishi Gurnani — Research, Code, & Ideas",
+	url: "https://<your-github-username>.github.io/<your-repo>/",
+	language: "en",
+	description: "A personal blog showcasing my research, software, and professional journey.",
+	author: {
+		name: "Dr. Rishi Gurnani",
+		email: "rishipgurnani@gmail.com",
+		url: "https://rishigurnani.wordpress.com/"
+	}
+}
