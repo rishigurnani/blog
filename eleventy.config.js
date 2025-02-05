868f59122d6d8f0ec80e69811cd81cbb66406a42
rishigurnani <rgurnani96@gmail.com>
2025-02-05 15:47:47 -0500

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -117,7 +117,6 @@ export default async function(eleventyConfig) {
 
 export const config = {
 	// Control which files Eleventy will process
-	// e.g.: *.md, *.njk, *.html, *.liquid
 	templateFormats: [
 		"md",
 		"njk",
@@ -125,14 +124,8 @@ export const config = {
 		"liquid",
 		"11ty.js",
 	],
-
-	// Pre-process *.md files with: (default: `liquid`)
 	markdownTemplateEngine: "njk",
-
-	// Pre-process *.html files with: (default: `liquid`)
 	htmlTemplateEngine: "njk",
-
-	// These are all optional:
 	dir: {
 		input: "content",          // default: "."
 		includes: "../_includes",  // default: "_includes" (`input` relative)
@@ -140,16 +133,6 @@ export const config = {
 		output: "_site"
 	},
 
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
-
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
-
-	// pathPrefix: "/",
+	// Set the pathPrefix to match your GitHub Pages URL
+	pathPrefix: "/blog/"
 };
